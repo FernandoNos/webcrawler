fecff07c8469f4a7408dddf4b4af8066300dce60
Fernando Nos <fernando461@gmail.com>
2018-05-31 23:02:36 -0300

    Percorrendo todos os links (dentro de uma tag a por agora), que pertençam ao domínio sicredi.com.br

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -1,37 +1,30 @@
 import scrapy
-
+from scrapy import signals
+from scrapy import Spider
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
+    allowed_domains=['www.sicredi.com.br']
+    tree = {}
+    count = 0
+    root = None
 
     def start_requests(self):
-        allowed_domains=[
-        'https://www.sicredi.com.br/']
+        
         urls = [
            'https://www.sicredi.com.br/'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
-     
     def parse(self, response):
-
-        page = response.url.split("/")[-2]
-
-        with open('output/'+page+'.txt', 'wb') as f:
-            f.write('Page: '+page)
-            print response.headers
-
-            print response.headers['Content-Type']
-            if 'text/html' in response.headers['Content-Type']:
-                selectors =  response.selector.xpath('//a/@href')
-                for selector in selectors:
-                    url = selector.extract()
-                    if 'http' in url:
-                        f.write('  '+url+'\n')
-                        #yield scrapy.Request(url, callback=self.parse)
-            else:
-                f.write( 'Static Document: '+response.url+'\n')
-        f.close()
-
+        self.count+=1
+        print response.url
+        if 'text/html' in response.headers['Content-Type']:
+            selectors =  response.selector.xpath('//a/@href')
+            for selector in selectors:
+                url = selector.extract()
+                if 'http' in url:
+                    if self.allowed_domains[0] in url:
+                        yield scrapy.Request(url, callback=self.parse)
         
